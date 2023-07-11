68ccf71a7ecf67f775f2bbf2a535e48a00c643af
Harry Fox <hfox@protonmail.com>
2023-07-11 17:04:32 -0500

    init /receipts/:id/points endpoint

--- src/routes/receipts.router.ts
+++ src/routes/receipts.router.ts
@@ -2,8 +2,11 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/process", (req, res) => {
+router.post("/process", (req, res) => {
   res.send("init process");
 });
 
+router.get("/:id/points", (req, res) => {
+  res.send(req.params);
+});
 export default router;
